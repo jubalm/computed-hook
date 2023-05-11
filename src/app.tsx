5a7b28c0e81d16e8d9dccba50dfd4757d6661af9
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-05-11 22:50:48 +0800

    replication

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useComputed, useSignal } from "@preact/signals";
+import { useState } from "preact/hooks";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [hookValue, hookSetter] = useState("world");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+    <div style={{ position: "fixed", inset: "0" }}>
+      <div style={{ display: "grid", placeItems: "center", padding: "0 1em" }}>
+        <input
+          style={{ border: "1px solid #000" }}
+          value={hookValue}
+          onInput={(event) => hookSetter(event.currentTarget.value)}
+        />
+        <MyComponent hookValue={hookValue} />
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
+}
+
+function MyComponent(props: { hookValue: string }) {
+  const mySignal = useSignal("hello");
+  const derivedSignal = useComputed(
+    () => `${mySignal.value} ${props.hookValue}`
+  );
+  return <span>{derivedSignal}</span>;
 }
